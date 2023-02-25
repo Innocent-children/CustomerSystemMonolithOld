3cf63348f7947645cbabe730dd3edab211171fc1
innocent-children <55626758+Innocent-children@users.noreply.github.com>
2023-02-25 09:41:35 +0800

    完成CustomerStaffServiceImpl

--- customer-system/src/main/java/org/geekbang/projects/cs/service/impl/CustomerStaffServiceImpl.java
+++ customer-system/src/main/java/org/geekbang/projects/cs/service/impl/CustomerStaffServiceImpl.java
@@ -2,21 +2,39 @@ package org.geekbang.projects.cs.service.impl;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
 import org.geekbang.projects.cs.entity.staff.CustomerStaff;
+import org.geekbang.projects.cs.entity.tenant.OutsourcingSystem;
 import org.geekbang.projects.cs.infrastructure.exception.BizException;
 import org.geekbang.projects.cs.infrastructure.page.PageObject;
+import org.geekbang.projects.cs.integration.OutsourcingSystemClient;
 import org.geekbang.projects.cs.mapper.CustomerStaffMapper;
 import org.geekbang.projects.cs.service.ICustomerStaffService;
+import org.geekbang.projects.cs.service.IOutsourcingSystemService;
+import org.geekbang.projects.cs.servicebus.endpoint.CustomerStaffEndpoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CustomerStaffServiceImpl extends ServiceImpl<CustomerStaffMapper, CustomerStaff> implements ICustomerStaffService {
+    @Autowired
+    IOutsourcingSystemService outsourcingSystemService;
+
+    @Autowired
+    OutsourcingSystemClient outsourcingSystemClient;
+
+    @Autowired
+    CustomerStaffEndpoint customerStaffEndpoint;
+
     @Override
     public PageObject<CustomerStaff> findCustomerStaffs(Long pageSize, Long pageIndex) {
-        return null;
+        return getCustomerStaffPageObject(null, pageSize, pageIndex);
     }
 
     @Override
@@ -26,7 +44,25 @@ public class CustomerStaffServiceImpl extends ServiceImpl<CustomerStaffMapper, C
 
     @Override
     public PageObject<CustomerStaff> findCustomerStaffsByName(String staffName, Long pageSize, Long pageIndex) {
-        return null;
+        return getCustomerStaffPageObject(staffName, pageSize, pageIndex);
+    }
+
+    private PageObject<CustomerStaff> getCustomerStaffPageObject(String staffName, Long pageSize, Long pageIndex) {
+        LambdaQueryWrapper<CustomerStaff> queryWrapper = new LambdaQueryWrapper<>();
+//        queryWrapper.eq(CustomerStaff::getIsDeleted, false);
+
+        if (!Objects.isNull(staffName)) {
+            queryWrapper.like(CustomerStaff::getStaffName, staffName);
+        }
+        queryWrapper.orderByDesc(CustomerStaff::getCreateTime);
+
+        IPage<CustomerStaff> page = new Page<>(pageIndex, pageSize);
+        IPage<CustomerStaff> pagedResult = baseMapper.selectPage(page, queryWrapper);
+
+        PageObject<CustomerStaff> pagedObject = new PageObject<CustomerStaff>();
+        pagedObject.buildPage(pagedResult.getRecords(), pagedResult.getTotal(), pagedResult.getCurrent(), pagedResult.getSize());
+
+        return pagedObject;
     }
 
     @Override
@@ -41,17 +77,28 @@ public class CustomerStaffServiceImpl extends ServiceImpl<CustomerStaffMapper, C
 
     @Override
     public Boolean updateCustomerStaff(CustomerStaff customerStaff) {
-        return null;
+        //可以直接使用updateById(),传入一个Entity对象即可，不需要传入LambdaQueryWrapper
+        return this.update(customerStaff, new LambdaQueryWrapper<CustomerStaff>().eq(CustomerStaff::getId, customerStaff.getId()));
     }
 
     @Override
     public Boolean deleteCustomerStaffById(Long staffId) {
-        return null;
+        return this.removeById(staffId);
     }
 
     @Override
     public void syncOutsourcingCustomerStaffsBySystemId(Long systemId) {
+        //获取租户信息
+        OutsourcingSystem outsourcingSystem = outsourcingSystemService.findOutsourcingSystemById(systemId);
 
+        //根据租户远程获取客服信息
+//        List<CustomerStaff> customerStaffs = outsourcingSystemClient.getCustomerStaffs(outsourcingSystem);
+
+
+        List<CustomerStaff> customerStaffs = customerStaffEndpoint.fetchCustomerStaffs(outsourcingSystem);
+
+        //保存客服信息
+        saveOrUpdateBatch(customerStaffs);
     }
 
 }
